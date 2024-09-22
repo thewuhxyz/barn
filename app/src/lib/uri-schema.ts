5cf479da167e50086a35fcfe6adb021ce938bb90
chizor iwuh <thisischizor@gmail.com>
2024-09-22 11:34:20 +0100

    uri schema

---
+++ app/src/lib/uri-schema.ts
@@ -0,0 +1,45 @@
+export type ProfileURI = {
+	name?: string | null;
+	bio?: string | null;
+	image_url?: string | null;
+	website?: string | null;
+	github?: string | null;
+	twitter?: string | null;
+	discord?: string | null;
+	telegram?: string | null;
+	linkedin?: string | null;
+	additional_info?: string[];
+};
+
+export type ProjectURI = {
+	name?: string | null;
+	description?: string | null;
+	image_url?: string | null;
+	objectives?: string | null;
+	website?: string | null;
+	github?: string | null;
+	twitter?: string | null;
+	additional_info?: string[];
+};
+
+export type GrantProgramURI = {
+	name?: string | null;
+	description?: string | null;
+	image_url?: string | null;
+	additional_info?: string[];
+};
+
+export type GrantURI = {
+	name?: string | null;
+	description?: string | null;
+	discussion?: string | null;
+	objectives?: string[];
+	additional_info?: string[];
+};
+
+export type GrantMilestoneURI = {
+	name?: string | null;
+	description?: string | null;
+	pr?: string[];
+	additional_info?: string[];
+};
